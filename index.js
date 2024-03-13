2607c2420e4f0715f81322484c728a5b8551f8ac
daniel-sanpedro <dpsanpedro@yahoo.com>
2024-03-13 15:55:28 -0700

    <multiplication done>

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-cd
+// Unit Testing
+
+// 1. multiplication
+// test multiplication of positive numbers
+    expect multiplication(3,4) to be (12);
+
+// test multiplication of negative numbers
+    expect multiplication(-3,-4) to be (12);
+
+// test multiplication of mixed numbers
+    expect multiplication (-3,4) to be (-12);
+
+// test multiplication by zero
+    expect multiplication (0,5) to be (0);
+
+    
