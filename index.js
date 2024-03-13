e3f8624a2de0580937587ad6d15ff8b45b455037
daniel-sanpedro <dpsanpedro@yahoo.com>
2024-03-13 16:16:48 -0700

    <added concarOdd>

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Unit Testing
 
-// 1. multiplication
+// multiplication
 // test multiplication of positive numbers
     expect multiplication(3,4) to be (12);
 
@@ -10,7 +10,37 @@
 // test multiplication of mixed numbers
     expect multiplication (-3,4) to be (-12);
 
+// test multiplication of decimal numbers
+    Expect multiplication(2.5, 4) to be equal to 10.0;
+
 // test multiplication by zero
     expect multiplication (0,5) to be (0);
 
-    
+// test multiplication by a letter(non-numeric value) and number
+    expect multiplication("a", 3) to be an error
+
+// unexpected inputs such as a letter(non-numeric value) will result in an error 
+// the function will return both positive and negative numbers
+// the function will return decimal numbers
+
+// concatOdd
+
+// test should return an array
+Expect concatOdds([3, 2, 1], [9, 1, 1, 1, 4, 15, -1]) to be an array
+
+// test should return an array of odd numbers
+Expect concatOdds([3, 2, 1], [9, 1, 1, 1, 4, 15, -1]) to be equal to [-1, 1, 3, 9, 15]
+
+// test should return an empty array if both input arrays contain only even numbers
+Expect concatOdds([2, 4, 6], [8, 10, 12]) to be an empty array
+
+// test should return a sorted array of odd numbers
+Expect concatOdds([1, 3, 5], [5, 3, 1]) to be equal to [1, 1, 3, 3, 5, 5]
+
+// should return an array
+// should return an array of only odd numbers
+// odd numbers should be sorted and be in an ascending order
+// should return an empty array if both inputs are even numbers
+
+// function test
+
